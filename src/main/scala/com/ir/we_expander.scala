77dc90d2e456105fb7e9166591666888b1fd03bb
Neele <Neele.witte@student.uni-tuebingen.de>
2017-03-26 16:31:41 +0200

    huhu

--- src/main/scala/com/ir/we_expander.scala
+++ src/main/scala/com/ir/we_expander.scala
@@ -6,7 +6,11 @@ package com.ir
 object we_expander {
 
   def main(args : Array[String]) {
+    import scala.io.Source
     println( "Hello group member!" )
+    val wordEmbeddings = "/home/neele/Dokumente/InformationRetrieval/glove.6B/glove.6B.50d.txt"
+    val map = Source.fromFile(wordEmbeddings).getLines().map(el => (el.split(" ")(0), el.split(" ").tail.map(_.toFloat))).toMap
+    println("done")
   }
 
 }
