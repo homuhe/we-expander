afed6d86babe7f2d00e9d76ce89914ff16ff3e83
homuhe <homuhe@gmail.com>
2017-03-26 17:17:49 +0200

    gar nicht

--- src/main/scala/com/ir/we_expander.scala
+++ src/main/scala/com/ir/we_expander.scala
@@ -1,22 +1,33 @@
 package com.ir
 import scala.io.Source
 
-import scala.collection.mutable
-import scala.io.Source
-
 /**
   * Created by neele, holger on 26.03.17.
   */
 object we_expander {
 
+  var embeddings = Map[String, Array[Float]]()
+
+
+  def read_embeddings(input: String): Map[String, Array[Float]] = {
+    Source.fromFile(input).getLines()
+      .map(el => (el.split(" ")(0), el.split(" ")
+        .tail
+        .map(_.toFloat))).toMap
+  }
+
+
+  // calculating the L2-norm for unit length normalization
+  def L2Norm(vector: Vector[Float]): Float = Math.sqrt(vector.map(x => x*x).sum).toFloat
+
+
+
   def main(args : Array[String]) {
     println( "Hello group member!" )
 
-    val wordEmbeddings = args(0)
-    val map = Source.fromFile(wordEmbeddings).getLines()
-                                              .map(el => (el.split(" ")(0), el.split(" ")
-                                              .tail
-                                              .map(_.toFloat))).toMap
+    val input = args(0)
+    embeddings = read_embeddings(input)
+
     println("done")
   }
 
