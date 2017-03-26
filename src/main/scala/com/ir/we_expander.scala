d38a9e599dbafd2774008f715b4ccdf06710fef0
homuhe <homuhe@gmail.com>
2017-03-26 16:42:18 +0200

    faster map creation

--- src/main/scala/com/ir/we_expander.scala
+++ src/main/scala/com/ir/we_expander.scala
@@ -1,4 +1,5 @@
 package com.ir
+import scala.io.Source
 
 /**
   * Created by neele, holger on 26.03.17.
@@ -6,8 +7,8 @@ package com.ir
 object we_expander {
 
   def main(args : Array[String]) {
-    import scala.io.Source
     println( "Hello group member!" )
+
     val wordEmbeddings = args(0)
     val map = Source.fromFile(wordEmbeddings).getLines()
                                               .map(el => (el.split(" ")(0), el.split(" ")
