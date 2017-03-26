8e9d8583f35ec30f3f74de38497edbe260c522ab
homuhe <homuhe@gmail.com>
2017-03-26 16:31:46 +0200

    X.x

--- src/main/scala/com/ir/we_expander.scala
+++ src/main/scala/com/ir/we_expander.scala
@@ -1,5 +1,8 @@
 package com.ir
 
+import scala.collection.mutable
+import scala.io.Source
+
 /**
   * Created by neele, holger on 26.03.17.
   */
@@ -7,6 +10,20 @@ object we_expander {
 
   def main(args : Array[String]) {
     println( "Hello group member!" )
+
+
+
+    val embeddings = mutable.HashMap[String, Array[Float]]()
+
+    val  lines = Source.fromFile(args(0)).getLines()
+
+    for (line <- lines) {
+      val elements = line.split(" ")
+      val word = elements(0)
+      val embedding = elements.tail.map(_.toFloat)
+
+      embeddings.put(word, embedding)
+    }
   }
 
 }
